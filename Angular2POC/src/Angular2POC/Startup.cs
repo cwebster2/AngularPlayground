bb5e8a08fa2c1b9ab8c611ea46d89b2e7281f01d
Webster, Casey (CC) <CCWebster@dow.com>
2016-03-17 17:22:30 -0400

    Initial Commit

---
+++ Angular2POC/src/Angular2POC/Startup.cs
@@ -0,0 +1,67 @@
+﻿using Glimpse;
+using System;
+using System.Collections.Generic;
+//using System.Deployment.Internal;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Angular2POC
+{
+    public class Startup
+    {
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGlimpse();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app,
+            IHostingEnvironment env,
+            ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseIISPlatformHandler(options => options.AuthenticationDescriptions.Clear());
+            app.UseStaticFiles();
+            app.UseGlimpse();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute("spa-fallback", "{*anything}", new {controller = "Home", action = "Index"});
+                routes.MapWebApiRoute("defaultApi", "api/{controller}/{id?}");
+            });
+        }
+    }
+}
