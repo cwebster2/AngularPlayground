0f6a28fedb5ddb1e58aefa03bd01bdd814d1c66c
Casey Webster <casey.webster@gmail.com>
2016-03-18 00:53:38 -0400

    Add angular2 stuff

---
+++ Angular2POC/src/Angular2POC/Controllers/PartialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Angular2POC.Controllers
+{
+    public class PartialController : Controller
+    {
+        public IActionResult Message() => PartialView();
+
+        public IActionResult Numbers() => PartialView();
+    }
+}
